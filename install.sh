0fd9ef17ed859d71070a012c6395586714bcf9f9
Brad Fisher <bradallenfisher@gmail.com>
2015-09-11 11:34:43 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,3 +16,16 @@ systemctl start  mysqld.service
 
 # php
 yum install -y php56w-fpm
+# fix date timezone errors
+sed -i 's/;date.timezone=/date.timezone ="America/New York"/g' /etc/php.ini
+systemctl enable php-fpm.service
+systemctl start php-fpm.service
+
+#add the ProxyPassMatch
+sed -i 's/IncludeOptional conf/#IncludeOptional conf/g' /etc/httpd/conf/httpd.conf
+cat << EOF >> /etc/httpd/conf/httpd.conf
+<IfModule proxy_module>
+  ProxyPassMatch ^/(.*\.php(/.*)?)$ fcgi://127.0.0.1:9000/var/www/html/$1
+</IfModule>
+IncludeOptional conf.d/*.conf
+EOF
