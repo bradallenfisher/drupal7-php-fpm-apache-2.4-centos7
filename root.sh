f7feeeb3a24636fa08f1ebf0751022a16eb0b0fe
Brad Fisher <bradallenfisher@gmail.com>
2015-09-11 12:04:02 -0400

    Update root.sh

--- root.sh
+++ root.sh
@@ -36,3 +36,7 @@ sed -i 's/AddType text/#AddType text/g' /etc/httpd/conf.d/php.conf
 
 systemctl restart httpd.service
 systemctl restart php-fpm.service
+
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+ln -s /usr/local/bin/composer /usr/bin/composer
