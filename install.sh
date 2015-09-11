d7b84a0dda748f1a464a2cab32765af718e2f30b
Brad Fisher <bradallenfisher@gmail.com>
2015-09-11 11:01:56 -0400

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,15 @@
+# install apache
+yum install nano vim wget curl git httpd -y
+
+# get some repos
+rpm -Uvh https://mirror.webtatic.com/yum/el7/epel-release.rpm
+rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
+rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+# get latest mysql
+wget http://dev.mysql.com/get/mysql-community-release-el7-5.noarch.rpm
+yum localinstall mysql-community-release-el7-5.noarch.rpm -y
+yum update -y
+yum install mysql-community-server -y
+systemctl enable mysqld.service
+systemctl start  mysqld.service
