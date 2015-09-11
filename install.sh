058e2d8548c81089599622d6b7b3c41b413b9b10
Brad Fisher <bradallenfisher@gmail.com>
2015-09-11 11:43:05 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,7 +15,7 @@ systemctl enable mysqld.service
 systemctl start  mysqld.service
 
 # php
-yum install -y php56w-fpm
+yum install -y php56w php56w-fpm php56w-opcache php56w-cli php56w-common php56w-gd php56w-mbstring php56w-mcrypt php56w-pecl-apcu php56w-pdo php56w-xml php56w-mysqlnd
 # fix date timezone errors
 sed -i 's/;date.timezone=/date.timezone ="America/New York"/g' /etc/php.ini
 systemctl enable php-fpm.service
@@ -29,3 +29,8 @@ cat << EOF >> /etc/httpd/conf/httpd.conf
 </IfModule>
 IncludeOptional conf.d/*.conf
 EOF
+
+#comment out the addHandler mod_php stuff
+sed -i 's/AddHandler php5-script .php/#AddHandler php5-script .php/g' /etc/httpd/conf.d/php.conf
+sed -i 's/AddType text/#AddType text/g' /etc/httpd/conf.d/php.conf
+
