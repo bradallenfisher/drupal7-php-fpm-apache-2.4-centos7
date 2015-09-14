6bd3a6a69a2a219829279c9a1624684453f53b8c
Brad Fisher <bradallenfisher@gmail.com>
2015-09-13 21:57:54 -0400

    Update post-install.sh

--- install/post-install.sh
+++ install/post-install.sh
@@ -17,6 +17,9 @@ drush site-install --account-pass=admin --db-url=mysql://root@localhost:22/test 
 sudo chmod -R 755 /var/www/html/sites/default/files/
 sudo chown -R apache:apache /var/www/html/sites/default/files/
 
+#start the apache service now that we have an .htaccess file
+sudo systemctl start httpd.service
+
 drush cleanup -y
 drush cook d7adminux -y
 drush cook d7adminux -y
