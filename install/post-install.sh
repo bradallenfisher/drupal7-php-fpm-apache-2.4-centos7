6864439a91b26934e7e3898f55cd96f5f0ff0490
Brad Fisher <bradallenfisher@gmail.com>
2015-09-13 23:30:52 -0400

    Update post-install.sh

--- install/post-install.sh
+++ install/post-install.sh
@@ -23,6 +23,11 @@ sudo systemctl start httpd.service
 drush cleanup -y
 drush cook d7adminux -y
 drush cook d7adminux -y
-exec bash
 
+# Install RVM
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+\curl -sSL https://get.rvm.io | bash -s stable
+source /home/vagrant/.rvm/scripts/rvm
+
+exec bash
 echo "DUNZY!"
